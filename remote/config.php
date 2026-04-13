bcf23a9cca2cc8bf1fafa10009833754e23beb93
tumbleDryCode <21972937+tumbleDryCode@users.noreply.github.com>
2026-04-13 19:36:37 +0100

    clean up
    clean up

--- remote/config.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-$con = mysqli_connect("pdb1046.freehostingeu.com","3537280_incasa","casain01","3537280_incasa");
-	if (mysqli_connect_errno())
-	{
-		echo "Failed to connect to MySQL: " . mysqli_connect_error();
-	}
-	include("include/functions.php");
-?>
